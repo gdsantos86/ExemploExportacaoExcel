f82f65157c71a686c831414484a8f3d02724e827
Saga <Saga@DESKTOP-L912NRQ>
2024-05-21 12:27:57 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExemploExportacaoExcel.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
